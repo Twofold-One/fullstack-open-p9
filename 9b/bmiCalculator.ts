f0ceb6b9e65175cbb5fac33dd3ac7691a74b2c10
Twofold-One <evan_geras@mail.ru>
2022-01-08 23:14:21 +0700

    9b in progress

---
+++ 9b/bmiCalculator.ts
@@ -0,0 +1,24 @@
+export const calculateBmi = (height: number, weigth: number): string => {
+    const BMI = weigth / (height / 100) ** 2;
+
+    if (BMI < 16) {
+        return 'Underweight (Severe thinness)';
+    } else if (BMI > 16 && BMI < 16.9) {
+        return 'Underweight (Moderate thinness)';
+    } else if (BMI > 17 && BMI < 18.4) {
+        return 'Underweight (Mild thinness)';
+    } else if (BMI > 18.5 && BMI < 24.9) {
+        return 'Normal range';
+    } else if (BMI > 25.0 && BMI < 29.9) {
+        return 'Overweight (Pre-obese)';
+    } else if (BMI > 30.0 && BMI < 34.9) {
+        return 'Obese (Class I)';
+    } else if (BMI > 35.0 && BMI < 39.9) {
+        return 'Obese (Class II)';
+    }
+    return 'Obese (Class III)';
+};
+
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+console.log(calculateBmi(a, b));
---
+++ 9b/exerciseCalculator.ts
@@ -0,0 +1,30 @@
+export const calculateExercises = (
+    target: number,
+    hoursPerDay: Array<number>
+) => {
+    const periodLength = hoursPerDay.length;
+    const trainingDays = hoursPerDay.filter((day) => day).length;
+    const average =
+        hoursPerDay.reduce((prev, current) => prev + current, 0) /
+        hoursPerDay.length;
+    const success = average > target;
+    const rating =
+        average / target >= 0.95 ? 3 : average / target >= 0.75 ? 2 : 1;
+    const ratingDescription =
+        rating === 3 ? 'great' : rating === 2 ? 'could be better' : 'bad';
+
+    return {
+        periodLength,
+        trainingDays,
+        target,
+        average,
+        success,
+        rating,
+        ratingDescription,
+    };
+};
+
+const a = Number(process.argv[2]);
+const b = process.argv.slice(3).map((arg) => Number(arg));
+
+console.log(calculateExercises(a, b));
